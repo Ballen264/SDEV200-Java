0bfe6b11d7f68be80584046f82c64b6f138cc3e0
Ballen264 <ballen264@ivytech.edu>
2025-03-19 22:53:53 -0400

    Assignment 1 upload.
    Initial input.

---
+++ 6.9/Assignment 1/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    static void foot_meter(int integer) {
+        System.out.println("Feet to meters: " + (integer * 0.305) + " meters");
+    }
+
+    static void meter_foot(int integer) {
+        System.out.println("Meters to feet: " + (integer * 3.279) + " feet");
+    }
+
+    public static void main(String[] args) {
+        foot_meter(1);
+        foot_meter(2);
+        foot_meter(3);
+        foot_meter(4);
+        foot_meter(5);
+        foot_meter(6);
+        foot_meter(7);
+        foot_meter(8);
+        foot_meter(9);
+        foot_meter(10);
+        meter_foot(20);
+        meter_foot(25);
+        meter_foot(30);
+        meter_foot(35);
+        meter_foot(40);
+        meter_foot(45);
+        meter_foot(50);
+        meter_foot(55);
+        meter_foot(60);
+        meter_foot(65);
+    }
+}
